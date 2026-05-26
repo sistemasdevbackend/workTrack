01d1091a8a6141c33874a35783bd59fe3268adb2
sistemasdevbackend <sistemasdevbackend@gmail.com>
2026-05-26 13:19:36 -0600

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'sanborns.com.mx',
+  appName: 'WorkTrack',
+  webDir: 'dist'
+};
+
+export default config;
